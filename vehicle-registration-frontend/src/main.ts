3f1a3c640b7876c684318eda52a06ded19367d1f
kajonas <kajonas@yahoo.com>
2025-07-03 18:57:23 -0500

    Added missing project files

---
+++ vehicle-registration-frontend/src/main.ts
@@ -0,0 +1,18 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+
+import { VehicleFormComponent } from './app/components/vehicle-form/vehicle-form.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+bootstrapApplication(VehicleFormComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      FormsModule,
+      ReactiveFormsModule,
+      HttpClientModule
+    )
+  ]
+});

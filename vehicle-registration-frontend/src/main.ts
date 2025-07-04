13e0a8fe076d7bddce8d44d4ee76529fea9362f9
kajonas <kajonas@yahoo.com>
2025-07-03 21:36:00 -0500

    Fixed bugs on full version

--- vehicle-registration-frontend/src/main.ts
+++ vehicle-registration-frontend/src/main.ts
@@ -1,17 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
-import { VehicleFormComponent } from './app/vehicle-form/vehicle-form.component';
+import { VehicleFormComponent } from './app/components/vehicle-form/vehicle-form.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(VehicleFormComponent, {
   providers: [
     importProvidersFrom(
-      BrowserModule,
-      FormsModule,
-      ReactiveFormsModule,
-      HttpClientModule
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule
     )
   ]
 });
